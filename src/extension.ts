d8d07de3b99b387d4e9376ef1b43b8c35e0328c7
Markus Eckstein <markus.eckstein@gmx.de>
2016-10-03 16:54:31 +0200

    Added some unit tests

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,11 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
-import * as http from 'http';
 import { getKrautText } from './kraut-loader';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "kraut-ipsum-filler" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.insertKraut', () => {
-        // The code you place here will be executed every time your command is executed
         let editor = vscode.window.activeTextEditor;
         let currentPos = editor.selection.active;
         let line = editor.document.lineAt(currentPos.line).text;
@@ -53,12 +41,11 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
 
 
-function parseEmmet(text: string): ParseResult {
+export function parseEmmet(text: string): ParseResult {
     const regexWithNum = /.*(kraut\*(\d+)).*/gi; // searches for 'foo bar kraut*2';
     const regexWithoutNum = /.*(kraut).*/gi; // searches for 'foo bar kraut';
     let result = regexWithNum.exec(text);
@@ -80,7 +67,7 @@ function parseEmmet(text: string): ParseResult {
     return { numberOfKrauts, startPos, endPos };
 }
 
-function sanitizeResult(count:number) {
+export function sanitizeResult(count:number) {
     if (count < 0) {
         count = 0;
     } else if (count > 5) {
@@ -89,7 +76,7 @@ function sanitizeResult(count:number) {
     return count;
 }
 
-type ParseResult = {
+export type ParseResult = {
     numberOfKrauts: number;
     startPos: number;
     endPos: number;
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,9 +1,4 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
 
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
@@ -11,12 +6,58 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+suite("Kraut Filler Extension Tests", () => {
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("Emmet parser leaves non-kraut text alone", () => {
+        let result: myExtension.ParseResult = myExtension.parseEmmet('foo bar*5');
+        assert.equal(0, result.numberOfKrauts);
+        assert.equal(0, result.startPos);
+        assert.equal(0, result.endPos);
+    });
+
+
+    test("Emmet parser single paragraph", () => {
+        let result: myExtension.ParseResult = myExtension.parseEmmet('foo bar*5kraut');
+        assert.equal(1, result.numberOfKrauts);
+        assert.equal(9, result.startPos);
+        assert.equal(14, result.endPos);
+    });
+
+    test("Emmet parser multi paragraph TC1", () => {
+        let result: myExtension.ParseResult = myExtension.parseEmmet('foo bar*5kraut*1');
+        assert.equal(1, result.numberOfKrauts);
+        assert.equal(9, result.startPos);
+        assert.equal(16, result.endPos);
+    });
+
+    test("Emmet parser multi paragraph TC2", () => {
+        let result: myExtension.ParseResult = myExtension.parseEmmet('foo bar*5kraut*0');
+        assert.equal(0, result.numberOfKrauts);
+        assert.equal(9, result.startPos);
+        assert.equal(16, result.endPos);
+    });
+
+    test("Emmet parser multi paragraph TC3", () => {
+        let result: myExtension.ParseResult = myExtension.parseEmmet('foo bar*5kraut*6');
+        assert.equal(6, result.numberOfKrauts);
+        assert.equal(9, result.startPos);
+        assert.equal(16, result.endPos);
+    });
+
+    test("Emmet parser multi paragraph TC4", () => {
+        let result: myExtension.ParseResult = myExtension.parseEmmet('foo bar*5kraut*333');
+        assert.equal(333, result.numberOfKrauts);
+        assert.equal(9, result.startPos);
+        assert.equal(18, result.endPos);
+    });
+
+    test("Sanitize negative number", () => {
+        let result: number = myExtension.sanitizeResult(-17);
+        assert.equal(0, result);
+    });
+
+    test("Sanitize number > 5", () => {
+        let result: number = myExtension.sanitizeResult(6);
+        assert.equal(5, result);
     });
 });
